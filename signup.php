a5c4b01a5f7e47621ebca169f2bd056be9eadfb9
rheajoy28 <82762559+rheajoy28@users.noreply.github.com>
2022-02-05 09:29:05 +0800

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CS Face - Signup</title>
+</head>
+<body>
+	<center>
+		<br>
+		<div style="border:solid 1px #888788; height:500px; width:550px; border-radius: 10px; padding:10px">
+			<table border="0">
+				<tr>
+					<td style="width: 500px;" valign="top">
+						<font style="font-size: 30pt; font-family:Verdana;"><strong>Sign Up</strong></font>
+						<br/>
+						<font style="font-size: 13pt; font-family:Verdana;">It's quick and easy.</font><hr>
+						<br>
+						<input type="text" name="txtfname" placeholder="First Name" size="25.9" style="border:1px solid #888788; border-radius:5px; font-size:15px; padding:10px">
+						<input type="text" name="txtlname" placeholder="Last Name" size="27.9" style="border:1px solid #888788; border-radius:5px; font-size:15px; padding:10px">
+						<br><br>	
+						<input type="text" name="txtusername" placeholder="Username" size="60" style="border:1px solid #888788; border-radius: 5px; font-size:15px; padding: 10px">
+						<br><br>
+						<input type="password" name="textpassword" placeholder="Password" size="60" style="border:1px solid #888788; border-radius:5px; font-size:15px; padding:10px">
+						<br><br>
+						<font style="font-size: 13pt; font-family:Verdana;">Birthday:</font>
+						<input type="text" name="txtbirthday" placeholder="mm-dd-yyyy" size="49" style="border:1px solid #888788; border-radius:5px; font-size:15px; padding:10px">
+						<br><br>
+						<font style="font-size: 13pt; font-family:Verdana;">Gender:</font>
+						<input type="text" name="txtbirthday" placeholder="Male/Female/Custom" size="50" style="border:1px solid #888788; border-radius:5px; font-size:15px; padding:10px">
+						<br><br>
+						<input type="submit" name="cmdsignup" value="Sign Up" style="border: 1px solid #888788; background-color: #1DCF68; border-radius: 5px; color: #FFFFFF; font-size: 20px; padding: 10px; width: 300px; cursor: pointer;">
+						<br><br>
+						<div style="background-color: #B41DCF; border-radius:5px; color: #FFFFFF; padding:10px; text-align:center; width: 200px;">
+						<a href="index.php" style="color:#FFFFFF; text-decoration:none;"><font style="font-family:Verdana; font-size: 18px;">Back</font> 
+						</a>
+					</div>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</center>
+</body>
+</html>
